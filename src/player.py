7c9f6cceebd52e3b1ea1635cd61362ec551cac70
Kawsar Hussen <kawsarhussen@gmail.com>
2019-05-29 16:03:30 -0400

    Player class added

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player():
+    def __init__(self, name, room):
+      self.name = name
+      self.current_room = room
+
+    def change_name(self, name):
+      self.name = name
+      
+    def move_room(self, new_room):
+      self.current_room = new_room
+
+    def __str__(self):
+      return f"{self.name} is currently in the {self.current_room}" 
